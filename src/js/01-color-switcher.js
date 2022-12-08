01f524783bbb54d067593a0afabf5a23c49cf3f1
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2022-12-08 23:51:12 +0200

    1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.body;
+let interval = null;
+buttonStart.addEventListener('click', switchingStart)
+buttonStop.addEventListener('click', switchingStop)
 
+buttonStop.setAttribute('disabled', true);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function switchingStart() {
+    buttonStop.removeAttribute('disabled', false)
+    buttonStart.setAttribute('disabled', true)
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+function switchingStop() {
+    buttonStop.setAttribute('disabled', true)
+    buttonStart.removeAttribute('disabled', false)
+    clearInterval(interval);
+}
